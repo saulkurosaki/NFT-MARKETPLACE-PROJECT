e22de9b81b4a9653f874e0bf7208835fb4a1d33d
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-31 11:25:50 -0600

    Basic _app.js theme|layout Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/5abf0b774f.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
 
 export default App;
