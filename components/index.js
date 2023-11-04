85667b2fd70d533d08de5d2a560a62deb64f0947
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-04 11:35:46 -0600

    Input.jsx Complete Componet Done

--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as Button } from './Button';
 export { default as Banner } from './Banner';
 export { default as CreatorCard } from './CreatorCard';
 export { default as NFTCard } from './NFTCard';
+export { default as Input } from './Input';
