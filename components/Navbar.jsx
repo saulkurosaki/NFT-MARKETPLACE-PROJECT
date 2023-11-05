666c0fa2146abbf1860e5f0a53cb88117b1b5e7c
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-05 13:02:01 -0600

    Navbar.jsx Component Update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 
 import images from '../assets';
 import { Button } from '.';
+import { NFTContext } from '../context/NFTContext';
 
 const MenuItems = ({ isMobile, active, setActive }) => {
   const generateLink = (i) => {
@@ -41,9 +42,9 @@ const MenuItems = ({ isMobile, active, setActive }) => {
 };
 
 const ButtonGroup = ({ setActive, router }) => {
-  const hasConnected = true;
+  const { connectWallet, currentAccount } = useContext(NFTContext);
 
-  return hasConnected ? (
+  return currentAccount ? (
     <Button
       btnName="Create"
       classStyles="mx-2 rounded-xl"
@@ -57,7 +58,7 @@ const ButtonGroup = ({ setActive, router }) => {
     <Button
       btnName="Connect"
       classStyles="mx-2 rounded-xl"
-      handleClick={() => {}}
+      handleClick={connectWallet}
     />
   );
 };
