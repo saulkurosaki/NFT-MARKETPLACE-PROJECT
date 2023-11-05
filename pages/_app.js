9a9fcc46114a8348e04125f47baa62824f186671
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-05 13:02:26 -0600

    _app.js Component Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,24 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
+import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/5abf0b774f.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/5abf0b774f.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default App;
