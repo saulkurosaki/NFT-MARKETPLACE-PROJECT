b1e93f6e9eba30d770600c2b0f966497f16c0d01
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-13 23:50:05 -0600

    Searbar.jsx Fully Complete Component Update

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -35,7 +35,7 @@ const Searchbar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
           width={20}
           height={20}
           alt="search"
-          className={theme === 'light' && 'filter invert'}
+          className={theme === 'light' ? 'filter invert' : ''}
         />
         <input
           type="text"
@@ -56,7 +56,7 @@ const Searchbar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
           width={15}
           height={15}
           alt="arrow"
-          className={theme === 'light' && 'filter invert'}
+          className={theme === 'light' ? 'filter invert' : ''}
         />
 
         {toggle && (
