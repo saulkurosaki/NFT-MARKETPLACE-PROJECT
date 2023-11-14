a4161432406165f3132eb72f4811fa517fa31f63
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-14 02:15:54 -0600

    create-nft.js Fully Complete Page File Done

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -5,14 +5,14 @@ import Image from 'next/image';
 import { useTheme } from 'next-themes';
 
 import { NFTContext } from '../context/NFTContext';
-import { Button, Input } from '../components';
+import { Button, Input, Loader } from '../components';
 import images from '../assets';
 
 const CreateNft = () => {
   const [fileUrl, setFileUrl] = useState(null);
   const [formInput, setFormInput] = useState({ price: '', name: '', description: '' });
   const { theme } = useTheme();
-  const { uploadToIPFS, createNFT } = useContext(NFTContext);
+  const { uploadToIPFS, createNFT, isLoadingNft } = useContext(NFTContext);
   const router = useRouter();
 
   const onDrop = useCallback(async (acceptedFile) => {
@@ -33,6 +33,14 @@ const CreateNft = () => {
     ${isDragAccept && 'border-file-accept'}
     ${isDragReject && 'border-file-reject'}`
   ), [isDragActive, isDragAccept, isDragReject]);
+
+  if (isLoadingNft) {
+    return (
+      <div className="flexStart min-h-screen">
+        <Loader />
+      </div>
+    );
+  }
 
   return (
     <div className="flex justify-center sm:px-4 p-12">
