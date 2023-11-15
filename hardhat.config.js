e9eaa6b093c772007fa3b6f673440ed0056042a8
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-15 12:25:35 -0600

    Hardhat Config File Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,10 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [privateKey],
+    },
   },
   solidity: '0.8.4',
 };
